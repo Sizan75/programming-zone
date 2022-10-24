83f3e23d84765a073b1a5d806e2f34bb50fb2d1d
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-25 04:34:51 +0600

    register page design added

---
+++ src/components/Register/Register.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Lottie from 'lottie-react'
+import login from '../../assets/login.json'
+import { Form, Link } from 'react-router-dom';
+const Register = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+              
+              
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <Form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Full Name</span>
+                            </label>
+                            <input name='fullName' type="text" placeholder="full name here" className="input input-bordered input-success w-full max-w-xs" required/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input name='photoURL' type="text" placeholder="Photo Url" className="input input-bordered input-success w-full max-w-xs" required/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="email" className="input input-bordered input-success w-full max-w-xs" required/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered input-success w-full max-w-xs" required />
+                        </div>
+                        <div className="form-control mt-5">
+                            <button className="btn btn-primary">Login</button>
+                            <label className="label">
+                        <p><small>Already have an account?</small>
+                                    <Link to="/login" className="text-purple-700 link link-hover">Log In</Link>
+                                    </p>
+                                </label>
+                        </div>
+                    </Form>
+                    <button className="btn btn-primary mt-5">Login</button>
+                    <button className="btn btn-primary mt-5">Login</button>
+                </div>
+                <div className='relative lg:w-1/4 '>
+                <div className="w-full lg:w-4/5 lg:ml-auto h-48 sm:w-2/5 sm:h-36 ">
+                <Lottie animationData={login} loop={true} />
+                </div>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../../components/ErrorPage/ErrorPage";
+import Home from "../../components/Home/Home";
+import Login from "../../components/Login/Login";
+import Register from "../../components/Register/Register";
 import Main from "../../layout/Main/Main/Main";
 
 export const routes= createBrowserRouter([
@@ -8,7 +11,18 @@ export const routes= createBrowserRouter([
         element: <Main></Main>,
         errorElement: <ErrorPage></ErrorPage>,
         children:[
-
+            {
+                path:'/',
+                element: <Home></Home>
+            },
+            {
+                path:'/login',
+                element: <Login></Login>
+            },
+            {
+                path:'/register',
+                element: <Register/>
+            }
         ]
     }
 ])
