b7ff1e2d1ab73b79016771d707c299037dd754e6
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-26 03:20:47 +0600

    card of course added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Course = ({course}) => {
     const {picture,name, _id, duration,rating,price}=course
 
     return (
-        <div>
-            
+        <div className="card mt-5 card-compact w-96  shadow-xl">
+        <figure className='bg-slate-700'><img className='h-56 ' src={picture} alt="course" /></figure>
+        <div className="card-body bg-green-100  text-dark">
+          <h2 className="card-title">{name}</h2>
+          <p className=''>{duration}</p>
+          <h5 className='font-semibold'>${price}</h5>
+          <div className="card-actions justify-end">
+            <button  className="btn btn-success text-dark"><Link to={`/${_id}`}>More Details</Link></button>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -6,10 +6,17 @@ const Courses = () => {
     const courses= useLoaderData();
     console.log(courses)
     return (
-        <div>
+        <div className='grid grid-cols-4 gap-4  '>
+            <div className=''>
+
+            </div>
+            <div className='col-span-3'>
+            <div className='grid grid-cols-2 gap-4'>
             {courses.map(course =><Course key={course._id}
             course={course}
             ></Course>)}
+        </div>
+            </div>
         </div>
     );
 };
